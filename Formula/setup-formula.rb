85916febda35de581587aefee4a695cacaf04afd
steigr <me@stei.gr>
2026-01-17 23:51:43 +0100

    Polish
    Signed-off-by: steigr <me@stei.gr>

--- Formula/setup-formula.rb
+++ Formula/setup-formula.rb
@@ -2,7 +2,7 @@ class SetupFormula < Formula
 
   url "https://github.com/steigr/homebrew-taps.git", branch: "main"
   homepage "https://github.com/steigr/homebrew-taps"
-  version "0.1.0"
+  version "0.1.1"
 
   depends_on "antidote"
   depends_on "arduino-cli"
